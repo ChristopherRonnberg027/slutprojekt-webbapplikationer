80c57e2b22e020ef0ad8485e5fc1d9e7fd157e6b
Sebastian Alvarez <sleam91@gmail.com>
2020-06-04 08:53:48 +0200

    added products list with responsive grid display

---
+++ src/components/Product.vue
@@ -0,0 +1,86 @@
+<template>
+    <article class="product">
+        <router-link :to="'/products/'+product._id" class="image-container">
+            <img :src="require('@/assets/'+product.imgFile)" alt />
+        </router-link>
+
+        <div class="product-info">
+            <div class="title-price">
+                <p class="title">{{product.title}}</p>
+                <p class="price">{{product.price}} kr</p>
+            </div>
+            <div class="desc-buy">
+                <p class="short-desc">{{product.shortDesc}}</p>
+                <p class="buy" @click="addToCart">Add to Cart</p>
+            </div>
+        </div>
+    </article>
+</template>
+
+<script>
+export default {
+    name: "Product",
+    props: {
+        product: Object
+    },
+    methods: {
+        addToCart() {
+            this.$store.commit("addToCart", this.product);
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+@mixin flex-column {
+    display: flex;
+    flex-direction: column;
+}
+@mixin flex-row {
+    display: flex;
+    justify-content: space-between;
+}
+@mixin flex-row-end {
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+}
+
+.product {
+    font-size: 1.4rem;
+    @include flex-column;
+    .image-container {
+        border-radius: 5px;
+        background-color: #c4c4c4;
+        padding: 1rem 2rem 0;
+        @include flex-row-end;
+        min-height: 21rem;
+
+        img {
+            object-fit: cover;
+            width: 18rem;
+        }
+    }
+    .product-info {
+        @include flex-column;
+        .title-price,
+        .desc-buy {
+            @include flex-row;
+            .buy {
+                background: #58e0b7;
+                border: 1px solid #000000;
+                padding: 0 1rem;
+                border-radius: 5px;
+                &:hover {
+                    cursor: pointer;
+                    background: cornflowerblue;
+                }
+            }
+        }
+        .title,
+        .buy {
+            font-weight: bold;
+        }
+    }
+}
+</style>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,13 +1,42 @@
 <template>
-  <h1>productlist view</h1>
+    <main class="products">
+        <h1>CHECK OUT OUR PRODUCTS</h1>
+        <section class="product-list">
+            <product
+                v-for="(singleProduct,i) in productList"
+                v-bind:key="i"
+                v-bind:product="singleProduct"
+            />
+        </section>
+    </main>
 </template>
 
 <script>
+import products from "@/assets/products.json";
+import Product from "@/components/Product";
 export default {
-
-}
+    name: "Products",
+    components: {
+        Product
+    },
+    data() {
+        return {
+            productList: products
+        };
+    }
+};
 </script>
 
 <style lang="scss" scoped>
-
+.products {
+    h1 {
+        text-align: center;
+    }
+    .product-list {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(18rem, 1fr));
+        gap: 3rem;
+        margin: 1rem;
+    }
+}
 </style>
