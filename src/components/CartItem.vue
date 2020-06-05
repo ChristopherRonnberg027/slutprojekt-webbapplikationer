0ed9fd1383dcc00d4765125ffb11d3f2ddc64e5c
Sebastian Alvarez <sleam91@gmail.com>
2020-06-05 15:35:40 +0200

    Co-authored-by: ChristopherRonnberg027 <christopher.ronnberg027@gmail.com> shopping cart and cart item

---
+++ src/components/CartItem.vue
@@ -0,0 +1,38 @@
+<template>
+    <article class="container">
+        <section class="product-info">
+            <h4 class="title">{{cartItem.product.title}}</h4>
+            <p class="price">{{cartItem.product.price}} :-</p>
+        </section>
+        <section class="quantity-btn">
+            <p class="btn">&and;</p>
+            <p class="quantity">{{cartItem.quantity}}</p>
+            <p class="btn">&or;</p>
+        </section>
+    </article>
+</template>
+
+<script>
+export default {
+    name: "CartItem",
+    props: {
+        cartItem: Object
+    },
+    methods: {
+        addToCart() {
+             this.$store.dispatch("addToCart", this.cartItem.product);
+        },
+        removeFromCart() {
+            this.$store.dispatch("removeFromCart", this.cartItem.product);
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+article {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+</style>
--- src/store/getters.js
+++ src/store/getters.js
@@ -3,3 +3,11 @@ export const getCartLength = (state) => {
     return 6;
     // return state.cart.length;
 }
+
+export const cartTotalPrice = (state) => {
+    let total = 0
+    state.cart.forEach(cartItem => {
+      total += cartItem.product.price * cartItem.quantity;
+    });
+    return total;
+  }
