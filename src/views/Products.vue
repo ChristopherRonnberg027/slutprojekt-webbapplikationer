2c2d11c9a7e5aafba0423883197b606bd03136b6
Rikard Virta <rikard.virta90@gmail.com>
2020-06-07 19:20:40 +0200

    removed routing bug in productlist. User was not able to navigate to SingleProduct.vue

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -2,10 +2,10 @@
   <main class="products">
     <h1 class="ma-5">CHECK OUT OUR PRODUCTS</h1>
     <v-container>
-      <v-row justify="start">
+      <v-row>
         <v-col cols="12" sm="6" md="4" lg="3" v-for="product in products" :key="product._id">
           <v-card>
-            <v-img router :to="'/products/'+product._id" contain :src="require('@/assets/'+product.imgFile)"></v-img>
+            <v-img contain :src="require('@/assets/'+product.imgFile)"></v-img>
             <v-card-title>{{product.title}}</v-card-title>
             <v-card-subtitle>{{product.shortDesc}}</v-card-subtitle>
             <v-card-text>{{product.price}}</v-card-text>
@@ -13,6 +13,9 @@
               <v-btn @click="addToCart(product)">
                 <v-icon class="mr-1">add_shopping_cart</v-icon>
                 <span>Add to cart</span>
+              </v-btn>
+              <v-btn router :to="'/products/'+product._id">
+                <v-icon>search</v-icon>
               </v-btn>
             </v-card-actions>
           </v-card>
