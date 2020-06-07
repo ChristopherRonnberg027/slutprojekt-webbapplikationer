3dfca62237987c659980d3e02d6fb9393e5498e6
Rikard Virta <rikard.virta90@gmail.com>
2020-06-07 17:26:09 +0200

    implemented vuetify in MyAccount.vue. Order item id's are bugged.

--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -1,5 +1,5 @@
 <template>
-  <main>
+  <!-- <main>
     <h1>my account view</h1>
     <section class="user-details">
       <h2>User details</h2>
@@ -22,6 +22,64 @@
         </p>
       </article>
     </section>
+  </main>-->
+  <main>
+    <h1 class="ma-5">my account view</h1>
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="10" sm="8" md="6" lg="4">
+          <v-card class="d-flex flex-column">
+            <v-card-text>
+              <v-card-title class="headline text--primary">User details</v-card-title>
+              <p class="text--primary">
+                <b>Name:</b>
+                {{user.name}}
+              </p>
+              <p class="text--primary">
+                <b>Email:</b>
+                {{user.email}}
+              </p>
+              <p class="text--primary">
+                <b>Role:</b>
+                {{user.role}}
+              </p>
+              <p class="text--primary">
+                <b>City:</b>
+                {{user.adress.city}}
+              </p>
+              <p class="text--primary">
+                <b>Street:</b>
+                {{user.adress.street}}
+              </p>
+              <p class="text--primary">
+                <b>Zip:</b>
+                {{user.adress.zip}}
+              </p>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn @click="logout()">Logout</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-expansion-panels class="my-10">
+        <v-expansion-panel v-for="order in orders" :key="order._id">
+          <v-expansion-panel-header>Order id: {{order._id}}</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-card flat>
+              <v-card-text class="text--primary">
+                <p><b>Date:</b> {{new Date(order.timeStamp)}}</p>
+                <p><b>Status:</b> {{order.status}}</p>
+                <p><b>Total sum:</b> {{order.orderValue}}</p>
+                <p v-for="item in order.items" :key="item.id">
+                  <span><b>Item id:</b> {{item}}</span>
+                </p>
+              </v-card-text>
+            </v-card>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-container>
   </main>
 </template>
 
@@ -29,8 +87,8 @@
 export default {
   methods: {
     logout() {
-      this.$store.dispatch('logout');
-      this.$router.push({name: 'Home'});
+      this.$store.dispatch("logout");
+      this.$router.push({ name: "Home" });
     }
   },
   computed: {
@@ -42,30 +100,10 @@ export default {
     }
   },
   beforeCreate() {
-    this.$store.dispatch('getOrders');
+    this.$store.dispatch("getOrders");
   }
 };
 </script>
 
 <style lang="scss" scoped>
-main {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  h1 {
-    margin: 2rem 0;
-  }
-
-  section {
-    border: 1px solid red;
-    min-width: 400px;
-    margin: 2rem 0;
-
-    article {
-      border: 1px solid blue;
-      margin: 1.5rem 0;
-    }
-  }
-}
 </style>
